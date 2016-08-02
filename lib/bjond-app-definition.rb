26ff4798ca9187caaacca424192cd48d36d0aa42
blake <blake.rego@gmail.com>
2016-08-02 00:03:23 -0400

    automatically setting the hostname when initializing an app definition.

--- lib/bjond-app-definition.rb
+++ lib/bjond-app-definition.rb
@@ -6,6 +6,12 @@ module BjondApi
 
     def initialize()
       self.id = SecureRandom.uuid
+      self.configURL    = "http://#{self.get_hostname}/bjond-app/services"
+      self.rootEndpoint = "http://#{self.get_hostname}/bjond-app/services"
+    end
+
+    def get_hostname()
+      Rails.application.config.action_controller.default_url_options ? Rails.application.config.action_controller.default_url_options[:host] : nil || `hostname`
     end
   end
 end
