306594006394a6fea09a970d6e6d93546b2e6084
blake <blake.rego@gmail.com>
2016-08-03 17:07:09 -0400

    Modifying the fire event api method to include event ID as a parameter.

--- lib/bjond-api.rb
+++ lib/bjond-api.rb
@@ -14,6 +14,7 @@ module BjondApi
   # Registers the given integration_app with a bjond-server.
   def self.register_app(integration_app, bjondhost=ENV['BJOND_SERVER'])
     conn = Faraday.new(:url => bjondhost)
+    puts integration_app.to_json
     return conn.post do |req|
       req.url '/server-core/services/integrationmanager/register'
       req.headers['Content-Type'] = 'application/json'
@@ -23,11 +24,11 @@ module BjondApi
 
   ##
   # Fires events to all bjond_registrations
-  def fire_event(bjond_registration, payload)
+  def fire_event(bjond_registration, payload, event_id)
     services = BjondService.where(:bjond_registration_id => bjond_registration.id)
     connections = []
     services.each do |bjond_svc|
-      conn = Faraday.new(:url => bjond_svc.endpoint)
+      conn = Faraday.new(:url => bjond_svc.endpoint + "/#{event_id}")
       conn.post do |req|
         req.headers['Content-Type'] = 'application/json'
         req.body = BjondJwt::jwt_encode_payload(payload, bjond_registration)
